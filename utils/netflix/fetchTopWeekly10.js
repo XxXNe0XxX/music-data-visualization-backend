9c8ca9c5b2e18b6844c28d1bae8bd91e0a6869bf
XxXNe0XxX <jm82467913@gmail.com>
2026-04-24 00:15:29 -0500

    temporary remove csv download

--- utils/netflix/fetchTopWeekly10.js
+++ utils/netflix/fetchTopWeekly10.js
@@ -3,6 +3,6 @@ import processNetflixTop10 from "./netflixProcessor.js";
 
 export async function netflixJob() {
   schedule.scheduleJob("0 3 * * 2", async () => {
-    await processNetflixTop10();
+    // await processNetflixTop10();
   });
 }
